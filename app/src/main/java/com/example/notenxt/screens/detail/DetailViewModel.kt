dafdcbf14772112c84b7ff3da0a0d6e406ff0802
anandfromdelhi <anand.textme@gmail.com>
2023-03-13 19:07:45 +0530

    detailviewmodel done

--- app/src/main/java/com/example/notenxt/screens/detail/DetailViewModel.kt
+++ app/src/main/java/com/example/notenxt/screens/detail/DetailViewModel.kt
@@ -1,2 +1,97 @@
 package com.example.notenxt.screens.detail
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import com.example.notenxt.models.Notes
+import com.example.notenxt.repository.StorageRepository
+import com.google.firebase.Timestamp
+import com.google.firebase.auth.FirebaseUser
+
+class DetailViewModel(
+    private val repository: StorageRepository
+) : ViewModel() {
+    var detailUiState by mutableStateOf(DetailUiState())
+        private set
+
+    private val hasUser: Boolean
+        get() = repository.hasUser()
+
+    private val user: FirebaseUser?
+        get() = repository.user
+
+    fun onColorChange(colorIndex: Int) {
+        detailUiState = detailUiState.copy(colorIndex)
+    }
+
+    fun onTitleChange(title: String) {
+        detailUiState = detailUiState.copy(title = title)
+    }
+
+    fun onNoteChange(note: String) {
+        detailUiState = detailUiState.copy(note = note)
+    }
+
+    fun addNote() {
+        if (hasUser) {
+            repository.addNote(
+                userId = user!!.uid,
+                title = detailUiState.title,
+                description = detailUiState.note,
+                color = detailUiState.colorIndex,
+                timeStamp = Timestamp.now()
+
+            ) {
+                detailUiState = detailUiState.copy(noteAddedStatus = it)
+            }
+        }
+    }
+
+    fun setEditFields(note: Notes) {
+        detailUiState = detailUiState.copy(
+            colorIndex = note.colorIndex,
+            title = note.title,
+            note = note.description
+        )
+    }
+
+    fun getNote(noteId: String) {
+        repository.getNote(noteId, onError = {}) {
+            detailUiState = detailUiState.copy(selectNote = it)
+            detailUiState.selectNote?.let { it1 -> setEditFields(it1) }
+        }
+    }
+
+    fun updateNote(noteId: String) {
+        repository.updateNote(
+            title = detailUiState.title,
+            note = detailUiState.note,
+            noteId = noteId,
+            color = detailUiState.colorIndex
+        ) {
+            detailUiState = detailUiState.copy(updateNoteStatus = it)
+        }
+    }
+
+    fun resetNoteAddedStatus() {
+        detailUiState = detailUiState.copy(
+            noteAddedStatus = false,
+            updateNoteStatus = false
+        )
+    }
+
+    fun resetState(){
+        detailUiState = DetailUiState()
+    }
+
+}
+
+data class DetailUiState(
+    val colorIndex: Int = 0,
+    val title: String = "",
+    val note: String = "",
+    val noteAddedStatus: Boolean = false,
+    val updateNoteStatus: Boolean = false,
+    val selectNote: Notes? = null
+)
